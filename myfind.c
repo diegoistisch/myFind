8ef9d405f9c0ab783ebe2fd617fde1a79661aa3f
Diegoistisch <diego.traxler@gmail.com>
2025-09-22 15:14:14 +0200

    fixed argumentparsing  and search

--- myfind.c
+++ myfind.c
@@ -24,11 +24,9 @@ int main(int argc, char **argv)
         {
         case 'R':
             modeRecursive = true;
-            fprintf(modeRecursive ? stderr : stdout, "Recursive mode is not implemented yet.\n");
             break;
         case 'i':
             modeCaseInsensitive = true;
-            fprintf(modeCaseInsensitive ? stderr : stdout, "Case-insensitive mode is not implemented yet.\n");
             break;
         
         default:
@@ -43,41 +41,23 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    struct dirent *direntp;
-    DIR *dirp;
-    if ((dirp = opendir(argv[1])) == NULL)
-    {
-      perror("Failed to open directory");
-      return 1;
+    const char *searchpath = argv[optind];
+    fprintf(stderr, "OK: searchpath='%s', files=%d, -R=%d, -i=%d\n",
+            searchpath, argc - optind - 1, modeRecursive, modeCaseInsensitive);
+
+    DIR *dirp = opendir(searchpath);
+    if (dirp == NULL) {
+        perror("Failed to open directory");
+        return 1;
     }
 
-    while ((direntp = readdir(dirp)) != NULL)
-      printf("%s\n", direntp->d_name);
-    while ((closedir(dirp) == -1) && (errno == EINTR))
-      ;
-    /** 
-    const char *searchpath = argv[optind++];
-    fprintf(stderr, "OK: searchpath='%s', files=%d, -R=%d, -i=%d\n",
-            searchpath, argc - optind, modeRecursive, modeCaseInsensitive);
-
-    // Durchsuche das Verzeichnis
-    DIR *dirp = opendir(searchpath);
-    if ((dirp = opendir(argv[1])) == NULL)
-   {
-      perror("Failed to open directory");
-      return 1;
-   }
-
-    
     struct dirent *direntp;
-    
+
     while ((direntp = readdir(dirp)) != NULL) {
-        // Ignoriere . und ..
         if (strcmp(direntp->d_name, ".") == 0 || strcmp(direntp->d_name, "..") == 0)
             continue;
 
-        // Vergleiche mit allen gesuchten Dateinamen
-        for (int i = optind; i < argc; ++i) {
+        for (int i = optind + 1; i < argc; ++i) {
             int match;
             if (modeCaseInsensitive) {
                 match = (strcasecmp(direntp->d_name, argv[i]) == 0);
@@ -90,7 +70,6 @@ int main(int argc, char **argv)
         }
     }
     closedir(dirp);
-    */
 
     return 0;
 }
