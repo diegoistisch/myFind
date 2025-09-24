f252ed59dccc6e31bcf14bc6de45aa4cc4c091e8
Diegoistisch <diego.traxler@gmail.com>
2025-09-24 13:53:29 +0200

    fixed fork and zombie prevention

--- myfind.c
+++ myfind.c
@@ -9,13 +9,13 @@
 #include <errno.h>
 #include <limits.h>
 #include <sys/stat.h>
-#include <sys/stat.h>
+#include <sys/wait.h>
 
 static void usage(const char *prog) {
     fprintf(stderr, "Usage: %s [-R] [-i] searchpath filename1 [filename2 ...]\n", prog);
 }
 
-void search_files(const char *dirpath, char **filenames, int num_files, bool case_insensitive, bool recursive) {
+void search_files(const char *dirpath, const char *filename, bool case_insensitive, bool recursive) {
     DIR *dirp = opendir(dirpath);
     if (dirp == NULL) {
         perror("Failed to open directory");
@@ -24,31 +24,28 @@ void search_files(const char *dirpath, char **filenames, int num_files, bool cas
 
     struct dirent *direntp;
     while ((direntp = readdir(dirp)) != NULL) {
-        
+
         if (strcmp(direntp->d_name, ".") == 0 || strcmp(direntp->d_name, "..") == 0)
             continue;
 
         char full_path[PATH_MAX];
         snprintf(full_path, sizeof(full_path), "%s/%s", dirpath, direntp->d_name);
 
-        for (int i = 0; i < num_files; i++) {
-            int match;
-            if (case_insensitive) {
-                match = (strcasecmp(direntp->d_name, filenames[i]) == 0);
-            } else {
-                match = (strcmp(direntp->d_name, filenames[i]) == 0);
-            }
-            if (match) {
-                //printf("%s\n", full_path);
-            }
+        int match;
+        if (case_insensitive) {
+            match = (strcasecmp(direntp->d_name, filename) == 0);
+        } else {
+            match = (strcmp(direntp->d_name, filename) == 0);
+        }
+        if (match) {
+            printf("%d: %s: %s\n", getpid(), filename, full_path);
         }
 
         if (recursive) {
             struct stat statbuf;
-                if (stat(full_path, &statbuf) == 0 && S_ISDIR(statbuf.st_mode)) {
-                    search_files(full_path, filenames, num_files, case_insensitive, recursive);
-                }
-
+            if (stat(full_path, &statbuf) == 0 && S_ISDIR(statbuf.st_mode)) {
+                search_files(full_path, filename, case_insensitive, recursive);
+            }
         }
     }
     closedir(dirp);
@@ -83,11 +80,7 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    //Debug Output
     const char *searchpath = argv[optind];
-    fprintf(stderr, "OK: searchpath='%s', files=%d, -R=%d, -i=%d\n",
-            searchpath, argc - optind - 1, modeRecursive, modeCaseInsensitive);
-
     char absolute_searchpath[PATH_MAX];
     if (realpath(searchpath, absolute_searchpath) == NULL) {
         perror("Failed to resolve absolute path");
@@ -97,16 +90,18 @@ int main(int argc, char **argv)
     char **filenames = &argv[optind + 1];
     int num_files = argc - optind - 1;
 
-    for (int i = optind; i < argc; ++i) {
-        if(fork() == 0) {
-            // child process
-            // do something with argv[i]
-            printf("%d : %s : %s\n", getpid(), filenames[i], absolute_searchpath);
-            exit(0); // terminate child process
+    // Child processes
+    for (int i = 0; i < num_files; i++) {
+        if (fork() == 0) {
+            search_files(absolute_searchpath, filenames[i], modeCaseInsensitive, modeRecursive);
+            exit(0);
         }
     }
-    search_files(absolute_searchpath, filenames, num_files, modeCaseInsensitive, modeRecursive);
-    
-    
+
+    // Parent process
+    for (int i = 0; i < num_files; i++) {
+        wait(NULL);
+    }
+
     return 0;
 }
